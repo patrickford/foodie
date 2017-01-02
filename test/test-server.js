a697e741f7b3f4096ece1c2208953e1b8683ec2a
Patrick Ford <patrickford@msn.com>
2017-01-01 18:54:55 -0800

    Set up Travis CI

---
+++ test/test-server.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var expect = chai.expect;
+var app = server.app;
+
+chai.use(chaiHttp);
+
+describe('Server', function() {
+  it('should respond with code 200 on root GET request', function(done) {
+    chai.request(app)
+        .get('/')
+        .end(function(err, res) {
+          should.equal(err, null);
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
+        });
+  });
+});
