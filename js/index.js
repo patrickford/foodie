5b550353e537b688b244114b04ad64f10bdafff3
Patrick Ford <patrickford@msn.com>
2017-01-12 16:22:38 -0800

    Used ES6 syntax for function

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
 const foodie = angular.module("foodie", []);
 
-foodie.controller("mainCtrl", function($scope) {
+foodie.controller("mainCtrl", ($scope) => {
   $scope.welcome = "Welcome to Foodie2Foodie";
   console.log("loaded...");
 });
